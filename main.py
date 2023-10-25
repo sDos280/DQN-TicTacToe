0611a6094dbc5288b16f25932580c867a95520f8
דור שפירא <Dor.Shapira@outlook.com>
2023-10-25 15:05:25 +0300

    add y training

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import torch
 import random
 import AgentNN
 import TicTacToeGameAPI
+import sys
 
 # training consts
-episodes = 1000
+episodes = 100
 discount_factor = 1
 epsilon = 0.2
 
@@ -20,7 +21,11 @@ agent_x = AgentNN.AgentNN(True)
 agent_y = AgentNN.AgentNN(False)
 
 # training loop
-for episode in range(episodes):
+for episode_num, episode in enumerate(range(episodes)):
+    sys.stdout.write('\r')
+    # the exact output you're looking for:
+    sys.stdout.write(f"%{(episode_num + 1) / episodes * 100}")
+    sys.stdout.flush()
 
     # collect data
     while True:
@@ -44,17 +49,35 @@ for episode in range(episodes):
             D_y.add(AgentNN.Experience(current_state, executed_action, reward, next_state))
 
         if game.is_state_terminal():
-            game.print_state()
+            # game.print_state()
             game.clean()
             break
 
     # train x
     for experience in D_x:
+        game.state = experience.next_state
+
         if game.is_state_terminal():
-            y = experience.reward
+            target = experience.reward
         else:
             game.state = experience.next_state
-            y = experience.reward + discount_factor * agent_x.forward(game.state, agent_x.get_max_q_action(game))
+            target = experience.reward + discount_factor * agent_x.forward(game.state, agent_x.get_max_q_action(game))
 
-        loss = criterion(agent_x.forward(experience.current_state, experience.action), torch.tensor([y], dtype=torch.float))
+        loss = criterion(agent_x.forward(experience.current_state, experience.action), torch.tensor([target], dtype=torch.float))
         loss.backward()
+
+    # train y
+    for experience in D_y:
+        game.state = experience.next_state
+
+        if game.is_state_terminal():
+            target = experience.reward
+        else:
+            game.state = experience.next_state
+            target = experience.reward + discount_factor * agent_y.forward(game.state, agent_y.get_max_q_action(game))
+
+        loss = criterion(agent_y.forward(experience.current_state, experience.action), torch.tensor([target], dtype=torch.float))
+        loss.backward()
+
+
+    game.clean()
