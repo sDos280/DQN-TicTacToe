0b90470094255ea4f7b9b39e95fef0f2aacfebfe
דור שפירא <Dor.Shapira@outlook.com>
2023-10-28 00:41:38 +0300

    add calculate_epsilon

--- Consts.py
+++ Consts.py
@@ -1,6 +1,12 @@
+import math
+
 State = list[int, int, int, int, int, int, int, int, int]
 Action = list[int, int, int, int, int, int, int, int, int]
 
 episodes = 500000
 discount_factor = 1
 epsilon = 0
+
+
+def calculate_epsilon(turn: int) -> float:
+    return min(1 / (turn * turn * math.log(turn + 1)), 1.0)
