9354f17b69a26be28d525334110eb84b765ea022
דור שפירא <Dor.Shapira@outlook.com>
2023-11-18 10:33:38 +0200

    add AgentNN.py

---
+++ AgentNN.py
@@ -0,0 +1,18 @@
+import torch
+
+
+class AgentNN(torch.nn.Module):
+    def __init__(self):
+        super(AgentNN, self).__init__()
+        self.module = torch.nn.Sequential(
+            torch.nn.Linear(9 + 1, 128),  # 9 cells in a game + 1 number representing the action
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 1),
+        )
+
+    def forward(self, observation: torch.Tensor, action: torch.Tensor) -> torch.Tensor:
+        inin = torch.cat((observation, action), dim=1)
+
+        return self.module(inin)
